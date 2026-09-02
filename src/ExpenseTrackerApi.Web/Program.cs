15af4e98d354c610f1305a28e93cd702457fc2a8
Tamas Nagy <nagy411@gmail.com>
2026-09-02 15:50:11 +0200

    remove db retry logic

--- src/ExpenseTrackerApi.Web/Program.cs
+++ src/ExpenseTrackerApi.Web/Program.cs
@@ -29,15 +29,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<IAppDbContext, AppDbContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
-        sqlOptions.EnableRetryOnFailure(
-            maxRetryCount: 5,
-            maxRetryDelay: TimeSpan.FromSeconds(30),
-            errorNumbersToAdd: null
-        )
-    )
-);
+builder.Services.AddDbContext<IAppDbContext, AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddHttpContextAccessor();
 
